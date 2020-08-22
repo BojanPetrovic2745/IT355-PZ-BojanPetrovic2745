70b98ffb554b0f655c3981a5c3639924ffacbe40
Bojan Petrovic <bojanpetrovic.net96@gmail.com>
2020-08-22 18:24:13 +0200

    IT355 PZ

---
+++ src/main/java/met/local/It355PzBojanPetrovicApplication.java
@@ -0,0 +1,13 @@
+package met.local;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It355PzBojanPetrovicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It355PzBojanPetrovicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/met/local/It355PzBojanPetrovicApplicationTests.java
@@ -0,0 +1,13 @@
+package met.local;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It355PzBojanPetrovicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
